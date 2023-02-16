967cd49534f5c0402b18f4fa63c6de306d398c6f
Erick <efmz2000@outlook.es>
2023-02-16 16:49:32 -0600

    a

---
+++ app/src/main/java/com/example/prototipo1/Hospedaje.kt
@@ -0,0 +1,36 @@
+package com.example.prototipo1
+
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.LinearLayout
+import androidx.annotation.RequiresApi
+
+class Hospedaje : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.P)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_hospedaje)
+        var list = this.requireViewById<LinearLayout>(R.id.hotelsList);
+
+        val names = listOf("Mc Donalds", "KFC", "Soda del lago","Asetec","Soda Deportiva","Subway","La sangüchera","Moes",
+                "Mc Donalds", "KFC", "Soda del lago","Asetec","Soda Deportiva","Subway","La sangüchera","Moes")
+        for (i in names.indices) {
+            // Create TextView programmatically.
+            val btn =  Button(this)
+            btn.layoutParams = LinearLayout.LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT
+            )
+            btn.text = names[i];
+            btn.textSize = 32.0F
+
+            list.addView(btn);
+        }
+
+
+    }
+
+}
---
+++ app/src/main/java/com/example/prototipo1/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.prototipo1
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    /** Called when the user taps the Send button */
+    fun sendMessage(view: View) {
+        // Do something in response to button
+        val intent = Intent(this, Hospedaje::class.java).apply {
+        }
+        startActivity(intent)
+    }
+}
