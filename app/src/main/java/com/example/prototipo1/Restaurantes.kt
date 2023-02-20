e1186251decd5c54905d996bf3fa5237f5d60fb1
deylan sandoval <deylan6@gmail.com>
2023-02-20 10:13:20 -0600

    listo

---
+++ app/src/main/java/com/example/prototipo1/Restaurantes.kt
@@ -0,0 +1,11 @@
+package com.example.prototipo1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Restaurantes : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_restaurantes)
+    }
+}
